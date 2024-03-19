14c8641af7cf4b074dacc16b20e4564ed6955799
Okebright <122984717+Okebright@users.noreply.github.com>
2024-03-19 07:41:56 +0100

    add new file

---
+++ eduford/eduford.js
@@ -0,0 +1,20 @@
+// Toggle menu
+// const navLinks = document.getElementById("navLinks");
+
+// function showMenu(){
+//     navLinks.style.right = "0";    
+// }
+// function hideMenu(){
+//     navLinks.style.right = "-200px";    
+// }
+
+
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.display = "block";    
+}
+function hideMenu(){
+    navLinks.style.display = "none";    
+}
