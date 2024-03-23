7f47b94e1ba3a1f772f3dd5cf1f4881129c5d462
Okebright <122984717+Okebright@users.noreply.github.com>
2024-03-23 09:23:52 +0100

    Update

--- eduford/eduford.js
+++ eduford/eduford.js
@@ -18,3 +18,34 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.display = "none";    
 }
+
+
+
+// Function to be executed when the observed element intersects with the viewport
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+        observer.unobserve(entry.target); // Stop observing once animated
+      }
+    });
+  }
+  
+  // Options for the Intersection Observer
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1 // Change the threshold as needed
+  };
+  
+  // Create the Intersection Observer
+  const observer = new IntersectionObserver(handleIntersection, options);
+  
+  // Target element to observe
+  const target = document.querySelector('.animate');
+  
+  // Start observing the target element
+  if (target) {
+    observer.observe(target);
+  }
+  
